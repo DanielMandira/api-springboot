d7b7e20a25946f63c1e0fcd2e24c1318b167502c
Daniel Augusto Mandira <105872910+DanielMandira@users.noreply.github.com>
2025-05-15 21:00:00 -0300

    Criando rotas de Math controller

--- src/main/java/br/com/mandira/controllers/MathController.java
+++ src/main/java/br/com/mandira/controllers/MathController.java
@@ -1,18 +1,56 @@
 package br.com.mandira.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.mandira.SimpleMath;
 
 @RestController
 public class MathController {
+	SimpleMath math = new SimpleMath();
 	
-	@GetMapping
-	public String teste() {
-		return "endpoint carregado com sucesso!";
+	@GetMapping("/sum")
+	public String sum2() {
+		return "Digite os numeros na URL da seguinte forma /sum/10/20";
 	}
 	
-	@GetMapping("/segundo")
-	public String teste2() {
-		return "Segundo endpoint";
+	@GetMapping("/sum/{num1}/{num2}")
+	public Double sum(
+			@PathVariable Double num1,
+			@PathVariable Double num2
+			) {
+		
+		return math.sum(num1, num2);
 	}
+	
+	@GetMapping("/sub/{num1}/{num2}")
+	public Double sub(
+			@PathVariable Double num1,
+			@PathVariable Double num2
+			) {
+		
+		return math.subtraction(num1, num2);
+	}
+	
+	@GetMapping("/div/{num1}/{num2}")
+	public Double div(
+			@PathVariable Double num1,
+			@PathVariable Double num2
+			) {
+		
+		return math.division(num1, num2);
+	}
+	
+	@GetMapping("/mult/{num1}/{num2}")
+	public Double mult(
+			@PathVariable Double num1,
+			@PathVariable Double num2
+			) {
+		
+		return math.multiplication(num1, num2);
+	}
+	
+	
+
 }
